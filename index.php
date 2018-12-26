de876bcb958f58008628893930e48f50c2c62d78
TruongThiNgocMai <44835809+TruongThiNgocMai@users.noreply.github.com>
2018-12-26 18:08:33 +0700

    Add files via upload
    Đã tạo xong form rồi nhé.

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>LOGIN USER</title>
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <form method="post" action="index.html" class="login">
+    <p>
+      <label for="login">UserName:</label>
+      <input type="text" name="username" id="username" placeholder="UserName">
+    </p>
+
+    <p>
+      <label for="password">Password:</label>
+      <input type="password" name="password" id="password" placeholder="Password">
+    </p>
+
+    <p class="login-submit">
+      <button type="submit" class="login-button">Login</button>
+    </p>
+
+    <p class="forgot-password"><a href="Register.html">Forgot your password? Click here to Register</a></p>
+  </form>
+  
+  
+</body>
+</html>
